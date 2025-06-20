cff74bf527d78c4118a67c34e512ebeacecde38c
Le Wang <lewang@todaq-le-mba.local>
2025-06-19 22:54:21 -0400

    initial commit

---
+++ le-eager.el
@@ -0,0 +1,22 @@
+;;; le-eager.el ---  lexical-binding: t; -*-
+
+
+(provide 'highlight-codetags)
+
+(defvar highlight-codetags-keywords
+  '(("\\<\\(TODO\\|FIXME\\|BUG\\|XXX\\)\\>" 1 font-lock-warning-face prepend)
+    ("\\<\\(NOTE\\|HACK\\)\\>" 1 font-lock-doc-face prepend)))
+
+;;;###autoload
+(define-minor-mode highlight-codetags-mode
+  "Highlight codetags like TODO, FIXME..."
+  :global t
+  (if le-highlight-codetags-mode
+      (font-lock-add-keywords nil le-highlight-codetags-keywords)
+    (font-lock-remove-keywords nil le-highlight-codetags-keywords))
+
+  ;; Fontify the current buffer
+  (when (bound-and-true-p font-lock-mode)
+    (if (fboundp 'font-lock-flush)
+        (font-lock-flush)
+      (with-no-warnings (font-lock-fontify-buffer)))))
